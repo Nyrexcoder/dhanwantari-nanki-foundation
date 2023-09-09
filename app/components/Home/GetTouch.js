0ce8421d4310d5e5196dca45b6c70faa4c73f920
neeraj kumar <88071981+Nyrexcoder@users.noreply.github.com>
2023-09-09 15:37:06 +0530

    update some pages

--- app/components/Home/GetTouch.js
+++ app/components/Home/GetTouch.js
@@ -5,15 +5,15 @@ const GetTouch = () => {
       <h1 className="text-center font-bold text-4xl text-green-900 p-3">
         HEALTHY BODY HAVE HEALTHY MIND
       </h1>
-      <p className="text-center text-sm text-green-900 p-3">
+      {/* <p className="text-center text-sm text-green-900 p-3">
         If you are looking for the best paint online, look no further. Berger
         Express Painting is a service all-rounder in home painting,
         waterproofing and wood coating solutions. Enjoy a hassle-free paint job
         today!
-      </p>
+      </p> */}
       <div className="flex md:flex-row flex-col w-full">
         <div className="border w-full md:w-[100%]">          
-        <img src="banner1.jpg" />
+        <img src="banner1.jpg" alt="banner" />
         </div>
       </div>
     </div>
