e1022207ed1c92992ee6bb56290a08ce32c510e7
neeraj kumar <88071981+Nyrexcoder@users.noreply.github.com>
2023-09-09 16:24:26 +0530

    update component location

--- app/components/Service.js
+++ app/components/Service.js
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react'
-import ServiceCard from '../../card/ServiceCard';
+import ServiceCard from './ServiceCard';
 const Service = () => {
     const services = [
         {
