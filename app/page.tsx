f272d828e61d33f47e2ca654ee64c51f832e7c25
neeraj kumar <88071981+Nyrexcoder@users.noreply.github.com>
2023-09-09 16:19:40 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,17 @@ import About from './components/about/About';
 import Counter from './components/Counter';
 import Footer from './components/Footer';
 import HeroSlider from './components/HeroSlider';
+import HomeTab from './components/HomeTab';
+import GetTouch from './components/GetTouch';
+import Service from './components/Service';
 export default function Home() {
   return (
     <>
     <HeroSlider/>
+    <HomeTab/>
+    <GetTouch/>
     <About/>
+    <Service/>
     <Counter/>
     <Footer/>
     </>
